BayLibre/android_kernel_common

ANDROID: mm: add vendor hook in fault and read file
Add a vendor hook to notify vendor module fault and read events.

Bug: 351175506
Change-Id: I4c46e9e00aa5f5555fd42a6b0815563497658b34
Signed-off-by: Sooyong Suk <s.suk@samsung.corp-partner.google.com>

mm/memory.c
@@ -4949,6 +4949,8 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 		ret = do_fault_around(vmf);
 		if (ret)
 			return ret;
+	} else {
+		trace_android_vh_do_read_fault(vmf, fault_around_pages);
 	}
 
 	ret = vmf_can_call_fault(vmf);

Kyuugeki/kernel-mtk-build-patch

Bug: 362665923
(cherry picked from commit a9867d872e24fbe658e05f32b770e4b36c6e3773)

mm/memory.c
@@ -4690,6 +4690,8 @@ static vm_fault_t do_read_fault(struct vm_fault *vmf)
 		ret = do_fault_around(vmf);
 		if (ret)
 			return ret;
+	} else {
+		trace_android_vh_do_read_fault(vmf, fault_around_bytes);
 	}
 
 	ret = vmf_can_call_fault(vmf);